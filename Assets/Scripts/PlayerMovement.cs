df78c9ad25b3b3bd7a8936180be9a38b34a2b250
LiamWalenius <liamwalenius@gmail.com>
2024-06-03 02:51:58 -0400

    added player movment

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,17 +4,65 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private new Rigidbody2D rigidbody;
+    private new BoxCollider2D collider;
+    [SerializeField] private LayerMask platformLayerMask;
+    private float horizontalInput;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpSpeed;
+    private bool canDoubleJump;
+
+    private bool IsGrounded => Physics2D.BoxCast
+        (
+            collider.bounds.center,
+            collider.bounds.size,
+            0,
+            Vector2.down,
+            0.1f,
+            platformLayerMask
+        );
     
-    
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if(IsGrounded)
+        {
+            canDoubleJump = true;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            TryJump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.velocity = new Vector2(horizontalInput * moveSpeed, rigidbody.velocity.y);
+    }
+
+    private void TryJump()
+    {
+        if(IsGrounded)
+        {
+            Jump();
+        }
+        else if(canDoubleJump)
+        {
+            canDoubleJump = false;
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpSpeed);
     }
 }
