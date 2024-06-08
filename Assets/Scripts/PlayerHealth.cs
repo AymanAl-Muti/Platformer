3f077b0c646ed210eace0e2d70974764d4c5eb00
LiamWalenius <liamwalenius@gmail.com>
2024-06-08 01:36:52 -0400

    improved healthbar

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,10 +1,11 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     private Slider healthBar;
-    private readonly float maxHealth = 100;
+    [SerializeField] private float maxHealth;
     private float currentHealth;
 
     private void Awake()
@@ -16,16 +17,43 @@ public class PlayerHealth : MonoBehaviour
     {
         healthBar = GameObject.Find("HealthBar").GetComponent<Slider>();
     }
-   
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            TakeDamage(10);
+        }
+
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            Heal(10);
+        }
+    }
+
     public void TakeDamage(float amount)
     {
         currentHealth -= amount;
+        if(currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Die();
+        }
         healthBar.value = currentHealth / maxHealth;
     }
 
     public void Heal(float amount)
     {
         currentHealth += amount;
+        if(currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
         healthBar.value = currentHealth / maxHealth;
+    }
+
+    private void Die()
+    {
+        Debug.Log("The player died");
     }
 }
